171c2a84d93fbe9782c610bbb4b9a4b5a2ee6093
vinitmishra17 <vinitkumarmishra540@gmail.com>
2025-12-09 13:11:38 +0530

    Add scroll event listener for navbar color change
    Implement navbar color change on scroll event.

---
+++ script.js
@@ -0,0 +1,10 @@
+// Navbar color change on scroll
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
